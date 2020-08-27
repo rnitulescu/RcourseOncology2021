112b32fce5cf9104c1dd537782e418eedadb96eb
Roy Nitulescu <roy.nitulescu@gmail.com>
2020-08-26 20:57:19 -0400

    Added content on if else

---
+++ src/R/module1/if_else.R
@@ -0,0 +1,13 @@
+## 6) If / Else
+myfn <- function(x) {
+	if (x == 0) {
+		print("Zero")
+	} else if (x == 1) {
+		print("One")
+	} else {
+		print("Not zero or one")
+	}
+}
+myfn(0)
+myfn(1)
+myfn(2)
