603b3f7802f9b46b303717bf57182283ad1fb41c
Roy Nitulescu <roy.nitulescu@gmail.com>
2020-09-12 19:32:40 -0400

    Added exercises for module 1

---
+++ exercises1.R
@@ -0,0 +1,33 @@
+#########################
+## Module 1: Exercises ##
+#########################
+
+###
+## Part I: In-class problems (solutions available)
+
+## Problem 1: Create a 4 by 4 matrix containing integer elements
+##   a) Retrieve the second column
+##   b) Retrieve the third row
+##   c) Retrieve all odd elements in the matrix
+##      (Hint: n %% 2 is equal to 1 for odd n and equal to zero for even n)
+
+## Problem 2: Use a for loop to add up the first five non-zero perfect squares
+##      (Hint: 1^2 = 1 and 2^2 = 4 are the first two non-zero perfect squares)
+
+## Problem 3: Using the data included with R called mtcars
+data(mtcars)
+##   a) Compute the mean mpg of all 4-cylinder vehicles
+##   b) Count the number of vehicles per cylinder size
+
+
+###
+## Part II: Take-home problems (no solutions given)
+
+## Problem 1: Create a 4 by 4 matrix containing character elements
+##            of lenghts varying between 3 and 5 characters
+##   a) Retrieve all elements strictly more than 3 characters long
+##      (Hint: nchar(x) returns the number of characters in the character variable x)
+
+## Problem 2: Use the functions Map and Reduce (and no loops)
+##            to add up the first five non-zero perfect squares
+
