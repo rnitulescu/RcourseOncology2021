7cee23f308aee8f0e5888e2611c4cbbd823a09eb
Roy Nitulescu <roy.nitulescu@gmail.com>
2020-10-27 20:24:50 -0400

    Added exercises and solutions for module 3

---
+++ exercises3.R
@@ -0,0 +1,22 @@
+#########################
+## Module 3: Exercises ##
+#########################
+
+###
+## Part I: In-class problems (solutions available)
+
+## Problem 1: Plot the power as a function of sample size for the following study design:
+##            - Normally distributed outcome
+##            - Two independent samples of the same size
+##            - Compared with a Student's t-test
+##            - Cohen's d is given below
+d <- 0.8
+
+## Problem 2: Write a script that performs randomization by permuted blocks,
+##            but where block size is also randomized.
+##            Check that treatment allocation is balanced.
+
+## Problem 3: Write a script that performs randomization by permuted blocks,
+##            but where there is stratification by sex (i.e., male and female)
+##            Check that treatment allocation is balanced for both males and females.
+
